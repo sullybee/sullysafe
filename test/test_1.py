cee55e9ec78e8aafda2195731818dd74ede5a6a7
SullyBee <Sullybeeapp@gmail.com>
2020-07-25 17:23:43 -0400

    Hack Presentation

--- test/test_1.py
+++ test/test_1.py
@@ -5,8 +5,8 @@ from time import sleep
 from google.cloud import firestore
 import firebase_admin
 from firebase_admin import credentials
+from google.cloud import firestore
 
-<<<<<<< HEAD
 
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="sullybee.json"
@@ -14,24 +14,25 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="sullybee.json"
 db = firestore.Client()
 
 
-coord1 = [42.098700,-75.912540]
+
 coord2 = [40.728226,-73.794853]
-lst = [coord1,coord2]
+coord1 = [40.7714030,-73.9195635]
+#coord3 = [40.7081,73.9571]
+#lst = [coord2,coord3,coord1]
+lst = [coord2,coord1]
 while(1):
 	i =1
 	for coord in lst: 
 		data1 = {
-
         		u'point': "angel"+str(i),
-			u'lat': coord[0],
-        		u'lon': coord[1]
+			u'geo': firestore.GeoPoint(coord[0],coord[1])
     		}
 
 		db.collection(u'data').document(u'datafile').set(data1)
 		sleep(10)
 		i = i + 1
 	i =1 
-=======
+'''
 cred = credentials.Certificate("./sullybee.json")
 firebase_admin.initialize_app(cred)
 
@@ -44,4 +45,4 @@ data = {
 }
 
 db.collection(u'data').document(u'datafile').set(data)
->>>>>>> ad28ebebb35f7610f73445b804c6effd80f36242
+'''
